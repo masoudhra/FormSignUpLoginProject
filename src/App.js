415810c9f4d1a85ff25087e72ab86ac9c54662b9
Masoud Ramezani <masoudhra@gmail.com>
2023-05-06 13:55:27 +1200

    create sign up form and login form

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from './compenents/SignUp';
+import Login from './compenents/Login';
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Navigate to="/signup" />} />
+
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
